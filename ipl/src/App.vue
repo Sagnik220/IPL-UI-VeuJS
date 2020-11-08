56aa2afe3931d82f49250dd25d216e3d49d9779c
Sagnik Mukherjee <sagnik2207@gmail.com>
2020-11-08 20:57:01 +0530

    update

--- ipl/src/App.vue
+++ ipl/src/App.vue
@@ -1,35 +1,33 @@
 <template>
   <div id="app">
     <header>
-      <Navbar/>
+      <Navbar />
     </header>
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar'
-
+import Navbar from './components/Navbar';
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-      Navbar
+    Navbar
   }
 }
 </script>
 
 <style>
 * {
-    box-sizing: border-box;
-    margin:0;
-    padding:0;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
-body{
-  font-family: 'monstserrat', sans-serif;
+body {
+  font-family: 'montserrat', sans-serif;
 }
-
-header{
-  width: 100ww;
-  background-color:#222;
-  padding:15px;
+header {
+  width: 100vw;
+  background-color: #222;
+  padding: 15px;
 }
 </style>
--- ipl/src/components/Navbar.vue
+++ ipl/src/components/Navbar.vue
@@ -1,42 +1,83 @@
 <template>
-    <nav>
-      <div class="menu-item"><a href="#">Home</a></div>
-      <div class="menu-item"><a href="#">Teams</a></div>
-      <div class="menu-item"><a href="#">Points Table</a></div>
-
-    </nav>
+  <nav>
+    <div class="menu-item"><a href="#">Home</a></div>
+    <Dropdown title="Teams" :items="Teams" />
+    <div class="menu-item"><a href="#">Points Table</a></div>
+    <div class="menu-item"><a href="#">More</a></div>
+  </nav>
 </template>
 
-
-
 <script>
+import Dropdown from './Dropdown';
 export default {
-    name:'navbar'
+  name: 'navbar',
+  components: {
+    Dropdown
+  },
+  data () {
+    return {
+      Teams: [
+        {
+          title: 'CSK',
+          link: '#'
+        },
+        {
+          title: 'DC',
+          link:'#'
+        },
+        {
+          title: 'KKR',
+          link: '#'
+        },
+        {
+          title: 'KXIP',
+          link: '#'
+        },
+        {
+          title: 'MI',
+          link: '#'
+        },
+        {
+          title: 'SRH',
+          link: '#'
+        },
+        {
+          title: 'RCB',
+          link: '#'
+        },
+        {
+          title: 'RR',
+          link: '#'
+        },
+      
+      ]
+    }
+  }
 }
 </script>
 
-<style scoped>
-nav{
-    display:flex;
-    align-items:center;
-    justify-content:center;
+<style>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-nav .menu-item{
-    color: #FFF;
-    padding: 10px 20px;
-    position:relative;
-    text-align: center;
-    border: bottom 3px solid transparent;
-    display: flex;
-    transition:0.4s;
-}
-nav .menu-item a{
-    color: inherit;
-    text-decoration:none;
+nav .menu-item {
+  color: #FFF;
+  padding: 10px 20px;
+  position: relative;
+  text-align: center;
+  border-bottom: 3px solid transparent;
+  display: flex;
+  transition: 0.4s;
 }
 nav .menu-item.active,
-nav .menu-item:hover{
-    background-color: #444;
-    border-bottom-color:#FF5858;
+nav .menu-item:hover {
+  background-color: #444;
+  border-bottom-color: #FF5858;
+}
+nav .menu-item a {
+  color: inherit;
+  text-decoration: none;
 }
 </style>
